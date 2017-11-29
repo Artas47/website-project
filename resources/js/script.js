7f4d84e32e0eb66dfbb76095c4f1bef9abbdee32
Artas47 <artasss47@gmail.com>
2017-11-29 20:08:11 +0100

    added a better navigation on mobile

--- resources/js/script.js
+++ resources/js/script.js
@@ -63,6 +63,49 @@ $('a[href*="#"]')
     }
   });
     
+    /*Animatrions oin scroll*/
+    
+    $('.js--wp-1').waypoint(function(direction){
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '80%'
+    });
+    
+    $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '80%'
+    });
+    
+    $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '80%'
+    });
+    
+    $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '80%'
+    });
+    
+    /*hidnig navigation*/
+    
+     $('.js--nav-icon').click(function (){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        nav.slideToggle(200);
+         if(icon.hasClass('ion-navicon-round')){
+             icon.addClass('ion-close-round');
+             icon.removeClass ('ion-navicon-round')
+         }else {
+              icon.addClass('ion-navicon-round');
+             icon.removeClass ('ion-close-round')
+         };
+         
+         
+        });
+    
 });
 
 
